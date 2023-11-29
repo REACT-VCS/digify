8f6cba2d63d54467ef185624a5e67e33966921b5
khalifalmahmud0 <code.khalif@gmail.com>
2023-11-29 15:15:03 +0600

    Working On Footer

--- app/(lib)/cn.js
+++ app/(lib)/cn.js
@@ -1,11 +1,6 @@
-import React from 'react';
-import {twMerge} from "tailwind-merge";
-import {clsx} from "clsx";
-
+import { twMerge } from "tailwind-merge";
+import { clsx } from "clsx";
 const cn = (...props) => {
-    return (
-       twMerge(clsx(props))
-    );
+	return twMerge(clsx(props));
 };
-
 export default cn;
