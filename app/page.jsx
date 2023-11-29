039af433ad878cc9001c2db434495221d741c58f
khalifalmahmud0 <code.khalif@gmail.com>
2023-11-29 16:48:41 +0600

    integrate google and local font

--- app/page.jsx
+++ app/page.jsx
@@ -4,19 +4,20 @@ import WorkLists from "@/app/(components)/workLists";
 import Statistics from "@/app/(components)/statistics";
 import FeaturedProjects from "@/app/(components)/featuredProjects";
 import FrontendLayout from "@/app/(layouts)/frontend";
-
 const Page = () => {
     return (
         <>
-           <FrontendLayout headerCss={"bg-black text-white"}>
-               <Hero/>
-               <Brands/>
-               <WorkLists/>
-               <Statistics/>
-               <FeaturedProjects/>
-           </FrontendLayout>
+            <FrontendLayout headerCss={"bg-black text-white"}>
+                <h1 className="text-9xl font-poppins">ABC</h1>
+                <h1 className="text-9xl font-avenir">ABC</h1>
+                <h1 className="text-9xl">ABC</h1>
+                <Hero />
+                <Brands />
+                <WorkLists />
+                <Statistics />
+                <FeaturedProjects />
+            </FrontendLayout>
         </>
     );
 };
-
 export default Page;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		fontFamily: {
+			poppins: ["var(--font-poppins)"],
+			avenir: ["var(--font-avenir)"],
+		},
+	},
+	plugins: [],
+};
